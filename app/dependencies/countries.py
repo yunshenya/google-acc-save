86a910d2ff303a76311d699bcbc043ad127771db
admin <3052405886@qq.com>
2025-08-11 16:06:36 +0800

    优化日志

--- app/dependencies/countries.py
+++ app/dependencies/countries.py
@@ -3,7 +3,7 @@ import csv
 from loguru import logger
 
 from config import default_proxy
-from app.dependencies.globals import ProxyManager
+from app.dependencies.proxy import ProxyManager
 
 manager = ProxyManager() ## 全局代理管理
 
