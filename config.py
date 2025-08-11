4d636f7f433fa0ea4238f15ccb5e5e271db0413a
admin <3052405886@qq.com>
2025-08-11 13:22:51 +0800

    添加环境变量配置

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+
 DATABASE_URL = "postgresql+asyncpg://postgres:1332@localhost:5432/google-manager"
 
-pad_code_list = ["ACP250605DG51WR4","ACP250417JIWWZSL","AC20250226GBVI6P","ACP250417FRB7H9K","ACP2504175KEOO32"]
+pad_code_list =  [] if DEBUG else ["ACP250605DG51WR4","ACP250417JIWWZSL","AC20250226GBVI6P","ACP250417FRB7H9K","ACP2504175KEOO32"]
 
 pkg_name = "com.aaee8h0kh.cejwrh616"
 
@@ -19,4 +25,5 @@ default_proxy = {
 temple_id_list = [614, 624]
 
 clash_install_url = "https://file.vmoscloud.com/userFile/b250a566f01210cb6783cf4e5d82313f.apk"
+
 script_install_url = "https://file.vmoscloud.com/userFile/d918272e71f99c7a1b835c5d3c138f4a.apk"
