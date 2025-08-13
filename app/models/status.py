ad46377bd8ddb867486331fb0ed70fe1514610d8
admin <3052405886@qq.com>
2025-08-13 21:08:19 +0800

    完善了获取云机的api

--- app/models/status.py
+++ app/models/status.py
@@ -1,11 +1,20 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class StatusResponse(BaseModel):
     pad_code: str
-    current_status: str
+    current_status: str | None = None
+    number_of_run: int
+    phone_number_counts: int
+    country: str | None = None
+    created_at: datetime
 
 
 class StatusRequest(BaseModel):
-    msg: str
-    data: StatusResponse
+    pad_code: str
+    current_status: str
+
+class GetOneCloudStatus(BaseModel):
+    pad_code: str
--- app/routers/status.py
+++ app/routers/status.py
@@ -1,16 +1,34 @@
-from fastapi import APIRouter
+from typing import List, cast
+
+from fastapi import APIRouter, HTTPException
+from sqlalchemy import ColumnElement
 
 from app.curd.status import update_cloud_status
-from app.models.status import StatusResponse, StatusRequest
+from app.models.status import StatusResponse, StatusRequest, GetOneCloudStatus
+from app.services.database import SessionLocal, Status
 
 router = APIRouter()
 
-@router.put("/status_update", response_model=StatusRequest)
-async def update_status_server(status_response: StatusResponse) -> StatusRequest:
-     status_request = await update_cloud_status(status_response.pad_code, status_response.current_status)
-     return status_request
+@router.put("/status_update", response_model=StatusResponse)
+async def update_status_server(status_request: StatusRequest) -> StatusRequest:
+     status_response = await update_cloud_status(status_request.pad_code, status_request.current_status)
+     return status_response
 
 
-@router.get("/status", response_model=dict)
-async def get_status_server() -> dict:
-     return {"status": "ok"}
+@router.get("/cloud_status", response_model=List[StatusResponse])
+async def get_status_server() -> List[StatusResponse]:
+     async with SessionLocal() as db:
+          from sqlalchemy import select
+          result = await db.execute(select(Status).order_by(cast(ColumnElement[bool], Status.id)))
+          status = result.scalars().all()
+          return status
+
+@router.post("/cloud_status", response_model=StatusResponse)
+async def get_one_cloud_status(one_cloud_status_request: GetOneCloudStatus) -> StatusRequest:
+     async with SessionLocal() as db:
+          from sqlalchemy import select
+          result = await db.execute(select(Status).filter(cast(ColumnElement[bool], Status.pad_code == one_cloud_status_request.pad_code)))
+          status = result.scalars().first()
+          if status is None:
+               raise HTTPException(status_code=404, detail="云机不存在")
+          return status
