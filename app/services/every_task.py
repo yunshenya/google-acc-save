347264c8d3f17bbdc98909fe920bf003fe42a35d
admin <3052405886@qq.com>
2025-08-21 10:27:25 +0800

    提高控制的精细度

--- app/services/every_task.py
+++ app/services/every_task.py
@@ -21,7 +21,7 @@ async def start_app_state(package_name, pad_code, task_manager):
             taskid= app_result["data"][0]["taskId"]
             match await check_padTaskDetail([taskid]):
                 case -1:
-                    logger.warning("正在一键新机")
+                    logger.warning(f"{pad_code}: 正在一键新机")
                     temple_id = random.choice(temple_id_list)
                     await replace_pad([pad_code], template_id=temple_id)
                     await task_manager.remove_task(pad_code)
@@ -42,7 +42,7 @@ async def start_app_state(package_name, pad_code, task_manager):
             taskid= app_result["data"][0]["taskId"]
             match await check_padTaskDetail([taskid]):
                 case -1:
-                    logger.warning("正在一键新机")
+                    logger.warning(f"{pad_code}: 正在一键新机")
                     temple_id = random.choice(temple_id_list)
                     await replace_pad([pad_code], template_id=temple_id)
                     await task_manager.remove_task(pad_code)
